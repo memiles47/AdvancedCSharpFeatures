fec9c737ab6dbdf689354e7c2b00c82bb03681a8
Michael E Miles <michael@miles-me.net>
2016-03-17 03:31:03 -0500

    First commit  - working lecture 3 by memory

---
+++ Lecture3/Lecture3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? value = null;
+
+            
+            int someNumber = value ?? default(int);
+            
+        }
+    }
+}
