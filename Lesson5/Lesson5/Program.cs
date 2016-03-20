76524236c954cb8c78dbdc819aa6d18a12558852
Michael E Miles <michael@miles-me.net>
2016-03-20 15:13:22 -0500

    Adjusting comments of lesson 3 and adding a new lesson 5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/*
+Lesson Five: Advanced C# Features
+Auto Properties
+
+*/
+
+namespace Lesson5
+{
+    #region Main Class for Program Entry
+
+    class Program
+    {
+        static void Main()
+        {
+            //Create a new instance of the Person_Explicit Class
+            Person_Explicit Person = new Person_Explicit();
+
+            //Set a value for the name string in the created class
+            Person.name = "Mike";
+
+            //Access the name variable
+            Console.WriteLine($"Person = {Person.name}");
+        }
+    }
+
+    #endregion
+
+    #region Person Class With Explicit Properties
+
+    public class Person_Explicit
+    {
+        private string _name = null;
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+
+    #endregion
+}
