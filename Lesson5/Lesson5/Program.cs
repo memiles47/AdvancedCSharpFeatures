e12d6be121b6a9b6361c4ce18ff0b5e173933601
Michael Miles <boat081062@gmail.com>
2016-03-21 06:22:22 -0500

    Auto Properties. Takes some thinking

--- Lesson5/Lesson5/Program.cs
+++ Lesson5/Lesson5/Program.cs
@@ -16,12 +16,21 @@ namespace Lesson5
         {
             //Create a new instance of the Person_Explicit Class
             Person_Explicit Person = new Person_Explicit();
+            Person_Auto Person_A = new Person_Auto();
+            Person_Field Person_F = new Person_Field();
+            Person_Immutable_Auto Person_IA = new Person_Immutable_Auto("Mike");
 
             //Set a value for the name string in the created class
             Person.name = "Mike";
+            Person_A.name = "Mike";
+            Person_F.name = "Mike";
+            //Person_IA.Name = "Michael"; //Shows an error as this field has become read only
 
             //Access the name variable
             Console.WriteLine($"Person = {Person.name}");
+            Console.WriteLine($"Person_A = {Person_A.name}");
+            Console.WriteLine($"Person_F = {Person_F.name}");
+            Console.WriteLine($"Person_IA = {Person_IA.Name}");
         }
     }
 
@@ -53,6 +62,65 @@ namespace Lesson5
     public class Person_Auto
     {
         public string name { get; set; }
+    }
+
+    #endregion
+
+    #region Person Class with Public Field
+
+    public class Person_Field
+    {
+        public string name;
+    }
+
+    #endregion
+
+    #region Immutable Person Class with Explicit Properties
+
+    public class Person_Immutable_Explicit
+    {
+        private readonly string _name = null;
+
+        public Person_Immutable_Explicit (string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Immutable Person Class with Explicit Properties
+
+    public class Person_Immutable_Private
+    {
+        public Person_Immutable_Private(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+
+    #endregion
+
+    #region Immutable Person Class with Auto Properties
+
+    public class Person_Immutable_Auto
+    {
+        public Person_Immutable_Auto(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
     }
 
     #endregion
