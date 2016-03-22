3ced957448c05ba09446ba6a1c2be01ddb9c3271
Michael Miles <boat081062@gmail.com>
2016-03-22 05:28:28 -0500

    Some great string methods to use in Lecture 6

---
+++ Lecture6_StringMethods/Lecture6_StringMethods/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Lecture6_StringMethods
+{
+    public static class StringMethods
+    {
+        #region GetFullName with explicit null check (GetFullName_ExplicitNull)
+
+        public static string GetFullName_ExplicitNull (string firstName, string lastName)
+        {
+            if (firstName == null || firstName.Length == 0)
+            {
+                return lastName;
+            }
+            else
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        #endregion
+
+        #region GetFullName with isnullorempty check (GetFullName_IsNullOrEmpty)
+
+        public static string GetFullName_IsNullOrEmpty(string firstName, string lastName)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                return lastName;
+            }
+            else
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        #endregion
+
+        #region GetFullName with explicit empty check (GetFullName_ExplicitEmpty)
+
+        public static string GetFullName_ExplicitEmpty(string firstName, string lastName)
+        {
+            if (firstName == null || firstName.Trim().Length == 0)
+            {
+                return lastName;
+            }
+            else
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        #endregion
+
+        #region GetFullName with IsNullOrWhiteSpace check (GetFullName_IsNullOrWhiteSpace)
+
+        public static string GetFullName_IsNullOrWhiteSpace(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                return lastName;
+            }
+            else
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        #endregion
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = null;
+
+            #region Explicit null check
+
+            result = StringMethods.GetFullName_ExplicitNull(null, "Miles");
+            Console.WriteLine($"Explicit Null: {result}");
+
+            #endregion
+
+            #region IsNullOrEmpty check
+
+            result = StringMethods.GetFullName_IsNullOrEmpty("", "Miles");
+            Console.WriteLine($"Is Null or Empty: {result}");
+
+            #endregion
+
+            #region Explicit Empty check
+
+            result = StringMethods.GetFullName_ExplicitEmpty("    ", "Miles");
+            Console.WriteLine($"Explicit Empty: {result}");
+
+            #endregion
+
+            #region IsNullOrWhiteSpace check
+
+            result = StringMethods.GetFullName_IsNullOrWhiteSpace("    ", "Miles");
+            Console.WriteLine($"Is null or white space: {result}");
+
+            #endregion
+
+            result = "MiKE MIlEs";
+
+            #region Case Sensitive check
+
+            Console.WriteLine($"Is this Mike? {result == "Mike Miles"}");
+
+            #endregion
+
+            #region UpperCase check
+
+            Console.WriteLine($"Is this Mike? {result.ToUpper() == "MIKE MILES"}");
+
+            #endregion
+
+            #region Case Insensitive check
+
+            Console.WriteLine($"Is this Mike? {string.Equals(result, "Mike Miles", StringComparison.OrdinalIgnoreCase)}");
+
+            #endregion
+
+        }
+    }
+}
