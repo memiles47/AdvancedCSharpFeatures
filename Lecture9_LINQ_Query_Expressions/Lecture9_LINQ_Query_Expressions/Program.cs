8a7537f5b14a797586d9ef9569850f63e40f22e7
Michael Miles <boat081062@gmail.com>
2016-03-23 05:28:14 -0500

    New Lexture using Linq.

---
+++ Lecture9_LINQ_Query_Expressions/Lecture9_LINQ_Query_Expressions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9_LINQ_Query_Expressions
+{
+    #region
+
+    public static class LinqQueryExpressions
+    {
+        public static void Demo()
+        {
+            string[] names = { "Billy", "Bob", "Brad", "David", "Duncan", "Everett", "Hanna", "Mark", "Michael", "Theo", "Tom", "Victor", "Wendy" };
+
+            #region Simple foreach loop
+
+            var results = new List<string>();
+            foreach (string name in names)
+            {
+                if (name.StartsWith("B"))
+                {
+                    results.Add(name.ToUpper());
+                }
+            }
+
+            #endregion
+
+            Console.WriteLine($"{string.Join(" ", results)}");
+        }
+    }
+
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqQueryExpressions.Demo();
+        }
+    }
+}
