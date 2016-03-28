1016a95c16bf1a7f82d8e3cb5890aa194efbca32
Michael Miles <boat081062@gmail.com>
2016-03-28 14:37:57 -0500

    Need a lot more experience

---
+++ Lecture1_Extention_Methods/Lecture1_Extention_Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1_Extention_Methods
+{
+    //public static void Demo()
+    //{
+    //    object obj = GetSomeOBject();
+
+    //    #region Convert object to string
+
+    //    Console.WriteLine($"Obj as string is: {obj.ToString()}");
+
+    //    #endregion
+
+    //    #region Convert obj to xml
+
+    //    Console.WriteLine($"Obj as xml is: {obj.ToXml()}");
+
+    //    #endregion
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We will not be using these until we are much more experienced with C#\n");
+        }
+    }
+}
