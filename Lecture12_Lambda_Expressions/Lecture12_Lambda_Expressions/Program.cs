8427f3f03f4f6c2508b9178076d1943079a8e625
Michael Miles <boat081062@gmail.com>
2016-03-29 04:26:17 -0500

    Lecture 12: Lambda Expressions.

---
+++ Lecture12_Lambda_Expressions/Lecture12_Lambda_Expressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/*
+Lecture12: Lambda Expressions
+*/
+
+namespace Lecture12_Lambda_Expressions
+{
+    public static class LambdaExpressions
+    {
+        #region Math Operation Delegate
+
+        public delegate int MathOperationDelegate(int number);
+
+        #endregion
+
+        #region Math Operation Method
+
+        private static int Multiply(int value)
+        {
+            return value * value;
+        }
+
+        #endregion
+
+        public static void Demo()
+        {
+            int value = 5;
+            int result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
