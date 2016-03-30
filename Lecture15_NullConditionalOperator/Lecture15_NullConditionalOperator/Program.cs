85f289770eb7f350d22d3d572ac60b37e3371dd4
Michael Miles <boat081062@gmail.com>
2016-03-30 05:23:28 -0500

    Lecture15: Null Conditional Operator

---
+++ Lecture15_NullConditionalOperator/Lecture15_NullConditionalOperator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture15_NullConditionalOperator
+{
+    public class NullConditionalOperator
+    {
+        #region Turncate Method
+
+        #endregion
+
+        #region Protected Truncate Method
+
+        #endregion
+
+        #region Padded Truncate Method
+
+        #endregion
+
+        #region Picknonnull Extension Method
+
+        #endregion
+
+        public void Demo()
+        {
+            #region Valid Truncate Call
+
+            #endregion
+
+            #region Invalid Truncate Call
+
+            #endregion
+
+            #region Padded Truncate Call
+
+            #endregion
+
+            #region Length Check
+
+            #endregion
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NullConditionalOperator MyNCO = new NullConditionalOperator();
+            MyNCO.Demo();
+        }
+    }
+}
