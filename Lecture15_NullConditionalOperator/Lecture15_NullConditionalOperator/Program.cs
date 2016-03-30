e0bca098eddfa518cda4fed30203295196e84240
Michael Miles <boat081062@gmail.com>
2016-03-30 06:12:33 -0500

    Null Conditional Operator is cool but my Truncate_Padded is not working. I need to figure that one out.

--- Lecture15_NullConditionalOperator/Lecture15_NullConditionalOperator/Program.cs
+++ Lecture15_NullConditionalOperator/Lecture15_NullConditionalOperator/Program.cs
@@ -5,15 +5,30 @@ namespace Lecture15_NullConditionalOperator
 {
     public class NullConditionalOperator
     {
-        #region Turncate Method
+        #region Truncate Method
+
+        private string Truncate(string value, int length)
+        {
+            return value.Substring(0, Math.Min(value.Length, length));
+        }
 
         #endregion
 
         #region Protected Truncate Method
 
+        private string Truncate_Protected (string value, int length)
+        {
+            return value?.Substring(0, Math.Min(value.Length, length));
+        }
+
         #endregion
 
         #region Padded Truncate Method
+
+        private string Truncate_Padded(string value, int length)
+        {
+            return value?.Substring(0, Math.Min(value.Length, length)).PadRight(Math.Max(value.Length - length, 0));
+        }
 
         #endregion
 
@@ -25,13 +40,29 @@ namespace Lecture15_NullConditionalOperator
         {
             #region Valid Truncate Call
 
+            string name = Truncate("Michael Miles", 9);
+            Console.WriteLine($"{name ?? "null"}");
+
             #endregion
 
             #region Invalid Truncate Call
 
+            //string name1 = Truncate(null, 9);
+            //Console.WriteLine($"{name1 ?? "null"}");
+
+            #endregion
+
+            #region Protected Truncate Call
+
+            string name1 = Truncate_Protected(null, 9);
+            Console.WriteLine($"{name1 ?? "The name is null"}");
+
             #endregion
 
             #region Padded Truncate Call
+
+            string name2 = Truncate_Padded("Michael Miles", 9);
+            Console.WriteLine($"{name2 ?? "The name is null"}, Length = {name2.Length}");
 
             #endregion
 
@@ -48,6 +79,8 @@ namespace Lecture15_NullConditionalOperator
         {
             NullConditionalOperator MyNCO = new NullConditionalOperator();
             MyNCO.Demo();
+
+            Console.WriteLine();
         }
     }
 }
