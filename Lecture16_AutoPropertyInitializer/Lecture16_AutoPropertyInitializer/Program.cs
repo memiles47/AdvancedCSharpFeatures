804ff6fe3f1edd8c305db1e70f5ea436e34d5671
Michael Miles <boat081062@gmail.com>
2016-04-04 05:42:05 -0500

    Creating the Lecture 16 solution file template to get started.

---
+++ Lecture16_AutoPropertyInitializer/Lecture16_AutoPropertyInitializer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+Lecture 16: AutoProperty Initializer
+*/
+
+namespace Lecture16_AutoPropertyInitializer
+{
+    #region class with Public Field
+
+
+
+    #endregion
+
+    #region class with Auto Property Initializer
+
+
+
+    #endregion
+
+    #region Immutable class with Auto Property Initializer
+
+
+
+    #endregion
+
+    #region Auto Property with complex initializer
+
+
+
+    #endregion
+
+    public static class AutoPropertyInitializer
+    {
+        #region Person Factory Property
+
+
+
+        #endregion
+
+        public static void Demo()
+        {
+            //Access Factory method
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
