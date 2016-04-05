7b4557721ac7e557c4c04a555418213ca08dd84f
Michael Miles <boat081062@gmail.com>
2016-04-05 04:04:50 -0500

    Lecture 17: Declaration Expressions

---
+++ Lecture17_DeclarationExpressions/Lecture17_DeclarationExpressions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+/*
+Lecture 17: Declaration Expressions
+*/
+
+namespace Lecture17_DeclarationExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            #region MyRegion
+
+            //input = Console.Read();
+            //while (input != 10)
+            //{
+            //    Console.Write((char)input);
+            //    input = Console.Read();
+            //}
+
+            #endregion
+
+            #region While loop with declaration expression
+
+            while ((input = Console.ReadLine().ToLower()) != "exit")
+            {
+                Console.WriteLine($"{input}");
+            }
+
+            Console.WriteLine($"{input}");
+
+            #endregion
+        }
+    }
+}
